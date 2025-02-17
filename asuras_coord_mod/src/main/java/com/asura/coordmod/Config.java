1610c5a137502abb20849fa0fb688a99099046eb
Asura <84172614+TrueAsura@users.noreply.github.com>
2025-02-17 15:35:08 +0200

    Add files via upload

--- asuras_coord_mod/src/main/java/com/asura/coordmod/Config.java
+++ asuras_coord_mod/src/main/java/com/asura/coordmod/Config.java
@@ -5,22 +5,19 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 public class Config {
     public boolean showCoordinates;
-    public boolean showDimensionIcon;
     public boolean showDayCounter;
 
-    public Config(boolean showCoordinates, boolean showDimensionIcon, boolean showDayCounter) {
+    public Config(boolean showCoordinates, boolean showDayCounter) {
         this.showCoordinates = showCoordinates;
-        this.showDimensionIcon = showDimensionIcon;
         this.showDayCounter = showDayCounter;
     }
 
     public Config() {
-        this(true, true, true); // Default values
+        this(true, true); // Default values
     }
 
     public static final Codec<Config> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.BOOL.fieldOf("showCoordinates").forGetter(config -> config.showCoordinates),
-            Codec.BOOL.fieldOf("showDimensionIcon").forGetter(config -> config.showDimensionIcon),
             Codec.BOOL.fieldOf("showDayCounter").forGetter(config -> config.showDayCounter)
     ).apply(instance, Config::new));
 }
--- asuras_coord_mod/src/main/java/com/asura/coordmod/CoordModClient.java
+++ asuras_coord_mod/src/main/java/com/asura/coordmod/CoordModClient.java
@@ -1,26 +1,16 @@
 package com.asura.coordmod;
 
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigCategory;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
+
 
 @Environment(EnvType.CLIENT)
 public class CoordModClient implements ClientModInitializer {
-    // Use Minecraft's default textures as fallback
-    private static final Identifier OVERWORLD_ICON = Identifier.of("minecraft", "textures/block/grass_block.png");
-    private static final Identifier NETHER_ICON = Identifier.of("minecraft", "textures/block/netherrack.png");
-    private static final Identifier END_ICON = Identifier.of("minecraft", "textures/block/end_stone.png");
-
     private static Config config;
 
     @Override
@@ -44,69 +34,14 @@ public class CoordModClient implements ClientModInitializer {
         int screenHeight = drawContext.getScaledWindowHeight();
 
         if (config.showCoordinates) {
-            // Format "X, Y, Z" in gold and coordinates in white
             String coords = String.format("§6X: §f%d §6Y: §f%d §6Z: §f%d", x, y, z);
-            drawContext.drawText(client.textRenderer, Text.literal(coords), screenWidth / 2 - 30, screenHeight - 40, 0xFFFFFF, true);
-        }
-
-        if (config.showDimensionIcon) {
-            Identifier icon = getDimensionIcon(client.world);
-            System.out.println("Rendering icon: " + icon); // Debug print
-            // Render the icon to the left of the coordinates
-            drawContext.drawTexture((identifier) -> RenderLayer.getGui(), icon, screenWidth / 2 - 70, screenHeight - 50, 0, 0, 16, 16, 16, 16);
+            drawContext.drawText(client.textRenderer, Text.literal(coords), screenWidth / 2 - 45, screenHeight - 60, 0xFFFFFF, true);
         }
 
         if (config.showDayCounter) {
             int days = (int) (client.world.getTimeOfDay() / 24000L);
-            // Format "Day" in bold
             String dayText = String.format("§lDay: %d", days);
             drawContext.drawText(client.textRenderer, Text.literal(dayText), 10, 10, 0xFFFFFF, true);
         }
-    }
-
-    private Identifier getDimensionIcon(World world) {
-        Identifier icon;
-        if (world.getRegistryKey() == World.OVERWORLD) {
-            icon = OVERWORLD_ICON;
-        } else if (world.getRegistryKey() == World.NETHER) {
-            icon = NETHER_ICON;
-        } else if (world.getRegistryKey() == World.END) {
-            icon = END_ICON;
-        } else {
-            icon = OVERWORLD_ICON; // Fallback to overworld icon for unknown dimensions
-        }
-        System.out.println("Current Dimension: " + world.getRegistryKey().getValue() + ", Icon: " + icon);
-        return icon;
-    }
-
-    public static Screen createConfigScreen(Screen parent) {
-        ConfigBuilder builder = ConfigBuilder.create()
-                .setParentScreen(parent)
-                .setTitle(Text.translatable("title.asuras_coord_mod.config"));
-
-        ConfigCategory category = builder.getOrCreateCategory(Text.translatable("category.asuras_coord_mod.general"));
-
-        category.addEntry(builder.entryBuilder()
-                .startBooleanToggle(Text.translatable("option.asuras_coord_mod.show_coordinates"), config.showCoordinates)
-                .setDefaultValue(true)
-                .setSaveConsumer(newValue -> config.showCoordinates = newValue)
-                .build());
-
-        category.addEntry(builder.entryBuilder()
-                .startBooleanToggle(Text.translatable("option.asuras_coord_mod.show_dimension_icon"), config.showDimensionIcon)
-                .setDefaultValue(true)
-                .setSaveConsumer(newValue -> config.showDimensionIcon = newValue)
-                .build());
-
-        category.addEntry(builder.entryBuilder()
-                .startBooleanToggle(Text.translatable("option.asuras_coord_mod.show_day_counter"), config.showDayCounter)
-                .setDefaultValue(true)
-                .setSaveConsumer(newValue -> config.showDayCounter = newValue)
-                .build());
-
-        // Save config when user closes the config screen
-        builder.setSavingRunnable(() -> CoordModConfig.saveConfig(config));
-
-        return builder.build();
     }
 }
